4fbdfaa48f9ca6f46bac7c7be51da808fa1540e7
maxwellberkowitz <15mab25@queensu.ca>
2018-01-31 19:56:55 -0500

    Semi-final changes to game

--- Assignment 1/src/Assn1_20019830.java
+++ Assignment 1/src/Assn1_20019830.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 
 public class Assn1_20019830 {
 
-	public static Random random = new Random(System.currentTimeMillis());
-	static Scanner input = new Scanner(System.in); // Scanner to be used to acquire user input in various methods
+	public static Random random = new Random(System.currentTimeMillis()); // Random utilized in numerous methods
+	static Scanner input = new Scanner(System.in); // Scanner to be used to acquire user input in numerous methods
 	
 	// Method generates random integers between 1 and 6 to represent dice rolling
 	public static int[] diceRoll(int dice[]) {
@@ -13,64 +13,76 @@ public class Assn1_20019830 {
 			return dice;
 	} // end diceRoll method
 	
-	// Method called to begin the game.
+	// Method called from main to begin the game. Will run until game terminates.
 	public static void startGame() {
-		int[] dice = new int[2];
-		int playerSum = 0;
+		int[] dice = new int[2]; // 1x2 array to represent the two dice
+		int playerSum = 0; // Initial player scores
 		int compSum = 0;
-		boolean playerTurn = true;
+		boolean playerTurn = true; // The user goes first
 		System.out.println("Welcome to pig! The first player to 100 or more points wins.\n"
 				+ "You (player) go first.");
-		while(playerSum < 100 && compSum < 100)
+		// Loop runs until end of game, changing player's turns and player's scores on each iteration.
+		while(playerSum < 100 && compSum < 100)  
 		{
 			System.out.println("\nPlayer's sum is: " + playerSum + ", Computer's sum is " + compSum + ".\n");
+			// if/else used to decide which player's turn it is
 			if(playerTurn == true)
 				playerSum += playerTurn(dice, playerSum);
 			else
 				compSum += compTurn(dice, compSum);
+			// alternating which player's turn it is for the next iteration
 			playerTurn = !playerTurn;
 		}
 		endGame(playerSum, compSum);
 	} // end startGame method
 	
+	// Method to decide the winner of the game. Final method run before returning to main and terminating.
 	public static void endGame(int playerSum, int compSum) {
 		if(playerSum > compSum)
-			System.out.println("You win! Player final score is " + playerSum + ". The computer's final score is " + compSum + ".");
+			System.out.println("You win! Your final score is " + playerSum + ". The computer's final score is " + compSum + ".");
 		else
-			System.out.println("The computer wins! Player final score is " + playerSum + ". The computer's final score is " + compSum + ".");
+			System.out.println("The computer wins! Your final score is " + playerSum + ". The computer's final score is " + compSum + ".");
 	} // end endGame method
 	
+	// Method intended to run until the player rolls a 1, or decides not to roll again.
+	// A sum is calculated for the current turn, and returned to startGame() to be added
+	// to the player's total score
 	public static int playerTurn(int dice[], int playerSum) {
 		System.out.println("Press Enter to roll.");
-		detectEnter();
-		dice = diceRoll(dice);
-		int turnSum = 0;
-		turnSum = sum(dice);
-		System.out.println("Player rolled a " + getWord(dice[0]) + " and a " + getWord(dice[1]) + ". Your total score is " + (turnSum + playerSum) + ".");
-		boolean reroll = rules(dice, turnSum, playerSum, "Player");
-		while(reroll) {
+		input.nextLine(); // Waiting for user to press enter
+		dice = diceRoll(dice); // Generating new dice values
+		int turnSum = sum(dice); // Initial turn sum
+		System.out.println("You rolled a " + getWord(dice[0]) + " and a " + getWord(dice[1]) + ". Your total score is " + (turnSum + playerSum) + ".");
+		// Loop created to manage the re-roll functionality. Breaks when rules() returns
+		// false, either due to rolling a 1 or deciding not to roll again
+		while(rules(dice, turnSum, playerSum, "Player")) {
 			System.out.println("Press Enter to roll.");
-			detectEnter();
-			dice = diceRoll(dice);
+			input.nextLine(); 
+			dice = diceRoll(dice); // Generating new dice values
+			// If sum() returns zero, this indicates a single 1 has been 
+			// rolled, and the turn must end with total sum of zero
 			if(sum(dice) == 0)
 				turnSum = 0;
 			turnSum += sum(dice);
-			System.out.println("Player rolled a " + getWord(dice[0]) + " and a " + getWord(dice[1]) + ". Your total score is " + (turnSum + playerSum) + ".");
-			reroll = rules(dice, turnSum, playerSum, "Player");
+			System.out.println("You rolled a " + getWord(dice[0]) + " and a " + getWord(dice[1]) + ". Your total score is " + (turnSum + playerSum) + ".");
 		}
 		return turnSum;
 	} // end playerTurn method
 	
+	// Method intended to run until the computer rolls a 1, or decides not to roll again.
+	// A sum is calculated for the current turn, and returned to startGame() to be added
+	// to the computer's total score.
 	public static int compTurn(int dice[], int compSum) {
-		dice = diceRoll(dice);
-		int turnSum = 0;
-		turnSum = sum(dice);
+		dice = diceRoll(dice); // Generating new dice values
+		int turnSum = sum(dice);
 		System.out.println("The computer rolled a " + getWord(dice[0]) + " and a " + getWord(dice[1]) + ".");
-		boolean reroll = rules(dice, turnSum, compSum, "The computer");
-		while(reroll) {
-			dice = diceRoll(dice);
+		// Loop created to manage the re-roll functionality. Breaks when rules() returns
+		// false, either due to rolling a 1 or deciding not to roll again
+		while(rules(dice, turnSum, compSum, "The computer")) {
+			dice = diceRoll(dice);  // Generating new dice values
 			System.out.println("The computer rolled a " + getWord(dice[0]) + " and a " + getWord(dice[1]) + ".");
-			reroll = rules(dice, turnSum + sum(dice), compSum, "The computer");
+			// If sum() returns zero, this indicates a single 1 has been 
+			// rolled, and the turn must end with total sum of zero
 			if(sum(dice) == 0)
 				turnSum = 0;
 			else 
@@ -79,27 +91,31 @@ public class Assn1_20019830 {
 		return turnSum;
 	} // end compTurn method
 	
-	public static void detectEnter() {
-		String enterKey = input.nextLine();
-		if(enterKey.isEmpty()) {
-			return;
-		}
-	} // end detectEnter method
-	
+	// Method used to account for the various possible die roll combinations, and their
+	// implications. Method returns a boolean value indicating whether or not the player
+	// will roll again, either by choice or by force.
 	public static boolean rules (int dice[], int sum , int totalSum, String player) {
+		// Case in which both dice are 1. Player must re-roll.
 		if(dice[0] == 1 && dice[1] == 1) {
 			System.out.println("Snake eyes! " + player + " must roll again!");
 			return true;
 		}
+		// Case in which dice are equal and not equal to 1. Player must re-roll.
 		else if(dice[0] == dice[1]) {
 			System.out.println("Doubles! " + player + " must roll again!");
 			return true;
 		}
+		// Case in which only a singular die is equal to 1. Does not catch
+		// case of both die being equal to 1, as this is tested earlier in
+		// the if-else string.
 		else if(dice[0] == 1 || dice[1] == 1) {
 			System.out.println("TURN OVER! Turn sum is zero!");
 			return false;
 		}
+		// Case to catch all other possible dice combinations
 		else {
+			// If the current turn is being played by the user, they are given
+			// the choice of deciding whether to play again
 			if(player.equals("Player")) {
 				String choice = getString("Roll again? (Enter 'y' or 'n'): ");
 				if(choice.equals("y")) {
@@ -109,11 +125,15 @@ public class Assn1_20019830 {
 					return false;
 			}
 			else
-				return compChoice(sum, totalSum);
+				return compChoice(sum, totalSum); // Method called if computer has choice regarding re-roll
 		}
-	}
+	} // end rules method
 	
+	// Method designed to decide whether computer will play again. Boolean is returned
+	// to indicate decision.
 	public static boolean compChoice(int turnSum, int compSum) {
+		// In the case that the computer has not yet reached a score of 100,
+		// and it has a turn score of less than 10, it will roll again.
 		if(turnSum < 10 && compSum < 100) {
 			System.out.println("The computer chose to play again.");
 			return true;
@@ -122,13 +142,16 @@ public class Assn1_20019830 {
 			System.out.println("The computer chose not to play again.");
 			return false;
 		}
-	}
+	} // end compChoice method
 	
+	// Method employed to determine whether the user wishes to re-roll
 	public static String getString(String prompt)
 	{
 		System.out.println(prompt);
 		String userString = new String();
 		boolean validInput = false;
+		// Loop created such that game will pause until the user enters a valid
+		// string indicating decision to re-roll
 		while(!validInput) {
 			userString = input.nextLine();
 			if(userString.equals("y") || userString.equals("n"))
@@ -140,28 +163,38 @@ public class Assn1_20019830 {
 		return userString;
 	} // end getString method
 	
+	// Method for calculating the value of a roll, according to game rules.
 	public static int sum(int dice[]) {
 		int sum;
+		// Double 1's
 		if(dice[0] == 1 && dice[1] == 1) {
 			sum = 25;
 		}
+		// Dice equal to each other and not equal to 1
 		else if(dice[0] == dice[1]) {
 			sum = 2*(dice[0] + dice[1]);
 		}
+		// Singular 1
 		else if(dice[0] == 1 || dice[1] == 1) {
 			sum = 0;
 		}
+		// All other rolls
 		else {
 			sum = dice[0] + dice[1];
 		}
 		return sum;
 	}
 	
+	// Method to translate integers into English word equivalents.
 	public static String getWord(int num) {
 		String nums [] = {"one", "two", "three", "four", "five", "six"};
 		return nums[num-1];
-	}
+	} // end getWord method
 	
+	// Main method left empty (aside from calling the beginning of the game).
+	// This was done intentionally such that if one wished to further develop
+	// this code, they could add content to the main program such as asking the
+	// user if they wished to play again.
 	public static void main(String[] args) {
 		startGame();
 	} // end main method
