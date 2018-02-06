fad2217ff3893ea5e0639854b0d2e0b0791f5cf4
maxwellberkowitz <berkowitzmax@gmail.com>
2018-02-06 01:34:18 -0500

    Working on Exercise 4

---
+++ Exercise 4/src/Experiment.java
@@ -0,0 +1,85 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Experiment {
+	
+	public static double[] dArray(int size) {
+		double[] array = new double[size];
+		for(int i = 0; i < size; i++)
+			array[i] = Math.random();
+		return array;
+	}
+	
+	public static long getSize(String filename) {
+		Path file = Paths.get(filename);
+		long filesize = 0;
+		try {
+			filesize = Files.size(file);
+		} catch (IOException e) {
+			System.out.println("I/O Exception:" + e);
+		}
+		return filesize;
+	}
+
+	public static void displayResults (String filename, long runTimeNanoseconds) {
+        System.out.printf("\n%-34s", filename);
+        System.out.printf("%8d bytes, saved in:", getSize(filename.trim()));
+        System.out.printf("%7.2f milliseconds.\n", runTimeNanoseconds * 1e-6);
+	}
+	
+	public static void textFileWriter(String filename, double[] array) {
+		Path file = Paths.get(filename);
+		try (BufferedWriter writer = Files.newBufferedWriter(file)){
+			for(double aVal : array)
+				writer.write(aVal + "\r\n");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void binaryFileWriter(String filename, double[] array) {
+		Path file = Paths.get(filename);
+		try (ObjectOutputStream binFileOut = new ObjectOutputStream(new BufferedOutputStream(
+						new FileOutputStream(file.toFile()))) ){
+			for(double e : array)
+				binFileOut.writeDouble(e);
+		} catch(FileNotFoundException err) {
+			System.out.println(err.getMessage());
+		} catch (IOException err) {
+			System.out.println(err.getMessage());
+		}
+	}
+	
+	public static void randomAccessBinaryOutput(String filename, double array[]) {
+		ByteBuffer buff = ByteBuffer.allocate(8*array.length);
+		DoubleBuffer dBuff = buff.asDoubleBuffer();
+		for (double val : array)
+			dBuff.put(val);
+		Path file = Paths.get(filename);
+		long bytesWritten;
+		StandardOpenOption readOpt = StandardOpenOption.READ;
+		StandardOpenOption writeOpt = StandardOpenOption.WRITE;
+		StandardOpenOption createOpt = StandardOpenOption.CREATE;
+		try(FileChannel fc = FileChannel.open(file, readOpt, createOpt, writeOpt)){
+			
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
