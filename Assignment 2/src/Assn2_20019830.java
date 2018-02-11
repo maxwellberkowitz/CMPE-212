eb30a885d739fe7337b2ef9891d1e114f1d85c55
maxwellberkowitz <berkowitzmax@gmail.com>
2018-02-10 23:28:34 -0500

    Finished most of the structure for the assignment. Able to write files for some motors. Motor 3 is the first motor that is throwing array out of bounds error

--- Assignment 2/src/Assn2_20019830.java
+++ Assignment 2/src/Assn2_20019830.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
 import java.nio.IntBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Files;
@@ -9,90 +15,111 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Assn2_20019830 {
-	
+
 	// Method calling all other methods and intended to be of highest level
 	// of abstraction besides main(). This method aims to, for a given motor,
 	// screen an excel file for surges and surge times, and write all surge
 	// instances to a text file. This method must be called individually for
 	// every motor being monitored.
 	public static void writeSurgeData(String readFileName, String writeFileName, int motorNumber) {
-		ArrayList<Integer> surgeTimes = getSurgeTimes(readFileName, motorNumber);
-		for (int i = 0; i < surgeTimes.size(); i++){
-			System.out.println(surgeTimes.get(i)); // temporary loop. just trying to see if correct values
-												   // have been retrieved.
-		}
+		double [] allVals = getCol(readFileName, motorNumber);
+		ArrayList<Integer> surgeTimes = getSurgeTimes(allVals);
+		double [] surgeAvgs = getAvg(allVals , surgeTimes);
+		System.out.println("The surge times are " + surgeTimes);
+		System.out.println("The surge averages are " + surgeAvgs[0] + " " + surgeAvgs[1] + " " + surgeAvgs[2]);
+		textFileWriter(writeFileName, surgeAvgs, surgeTimes);
 	}
-	
+
 	// Method will write a set of surge times and average values for a given
 	// array to a text file. The array argument given will be the output of
 	// the This method can only write to one file per call.
-	public static void textFileWriter(String filename, double[] surgeVals, double[] surgeTimes) {
-/*		Path file = Paths.get(filename);
-		try (BufferedWriter writer = Files.newBufferedWriter(file)){
-			for(double aVal : array)
-				writer.write(aVal + "\r\n");
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}*/
+	public static void textFileWriter(String writeFileName, double[] surgeAvgs, ArrayList<Integer> surgeTimes) {
+		try {
+			PrintWriter writer = new PrintWriter(writeFileName, "UTF-8");
+			writer.println("start (sec), finish (sec), current (amps)");
+			for(int i = 0; i < surgeAvgs.length; i++) {
+				writer.println(surgeTimes.get(i*2) + ", " + surgeTimes.get((i*2)+1) + ", " + surgeAvgs[i]);
+			}
+			writer.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
 	// Method will take array argument output from getSurgeTimes, calculate
-	// average values for each duration, and return them in a cronologically
+	// average values for each duration, and return them in a chronologically
 	// ordered array. The argument array size should be twice that of the
 	// returned array size.
-/*	public static double [] getAvg(double surgeVals[]) {
-		
-	}*/
-	
-	// Method will return an array with the first and last seconds of all surges
-	// in a given motor. First second will immediately precede last second of a 
-	// given surge. Array size will be an integer multiple of 2.
-	public static ArrayList<Integer> getSurgeTimes(String readFileName, int motorNumber) {
-		int [] allVals = new int[1000];
-		ByteBuffer buff = ByteBuffer.allocate(4 * 1000);
-		IntBuffer intBuff = buff.asIntBuffer();
-		Path file = Paths.get(readFileName);
-		ArrayList<Integer> surgeTimes = new ArrayList<Integer>();
-		if (Files.exists(file, LinkOption.NOFOLLOW_LINKS) && Files.isReadable(file)) {
-			StandardOpenOption readOpt = StandardOpenOption.READ;
-			try (FileChannel fc = (FileChannel.open(file, readOpt))) {
-				// As far as I understand, I need to loop through all 1000 values in the given motor
-				// column, and put these values in the buffer. But I have no idea how to reference the
-				// correct location starting location, or how to increment from there...
-/*				fc.position(0);
-				fc.read(buff);*/
-			} catch (IOException err) {
-				System.out.println("I/O Exception: " + err);
+	public static double[] getAvg(double allVals[], ArrayList<Integer> surgeTimes) {
+		double [] avgs = new double [surgeTimes.size()/2];
+		for(int i = 0; i < avgs.length; i ++) {
+			for(int j = surgeTimes.get(i*2); j <= surgeTimes.get((i*2)+1); j++) {
+				avgs[i] += allVals[j];
 			}
-			intBuff.get(allVals);
-			boolean firstSurgeTimeFound = false;
-			for(int i = 0; i < 1000; i++) {
-				if(1 < allVals[i] && firstSurgeTimeFound == false) {
-					surgeTimes.add(allVals[i]);
-					firstSurgeTimeFound = true;
-				}
-				else if(allVals[i] < 1 && firstSurgeTimeFound == true) {
-					surgeTimes.add(allVals[i]);
-					firstSurgeTimeFound = false;
-				}
-			}
+			avgs[i] = avgs[i]/(surgeTimes.get((i*2)+1)-(surgeTimes.get(i*2)-1));
 		}
-		else {
-			surgeTimes = null;
+		return avgs;
+	}
+
+	// Method will return an array with the first and last seconds of all surges
+	// in a given motor. First second will immediately precede last second of a
+	// given surge. Array size will be an integer multiple of 2.
+	public static ArrayList<Integer> getSurgeTimes(double[] allVals) {
+		ArrayList<Integer> surgeTimes = new ArrayList<Integer>();
+		boolean firstSurgeTimeFound = false;
+		for (int j = 0; j < 1000; j++) {
+			if (1 < allVals[j] && firstSurgeTimeFound == false) {
+				surgeTimes.add(j);
+				firstSurgeTimeFound = true;
+			} else if (allVals[j] < 1 && firstSurgeTimeFound == true) {
+				surgeTimes.add((j - 1));
+				firstSurgeTimeFound = false;
+			}
 		}
 		return surgeTimes;
 	}
-	
+
+	public static double[] getCol(String readFileName, int motorNumber) {
+		double[] allVals = new double[1000];
+		String line = null;
+		try {
+			FileReader fileReader = new FileReader(readFileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			int i = 0;
+			while ((line = bufferedReader.readLine()) != null) {
+				StringTokenizer st = new StringTokenizer(line);
+				for (int j = 0; j < motorNumber; j++)
+					st.nextToken();
+				String str = st.nextToken();
+				str = str.substring(0, str.length() - 1);
+				allVals[i] = Double.parseDouble(str);
+				i++;
+			}
+			bufferedReader.close();
+		} catch (NumberFormatException e) {
+			System.out.println("I/O Exception: " + e);
+		} catch (IOException e) {
+			System.out.println("I/O Exception: " + e);
+		}
+		return allVals;
+	}
+
 	public static void main(String[] args) {
-		writeSurgeData("Logger.csv", "Motor1.txt", 1);
-		/*writeSurgeData("Logger.csv", "Motor2.txt", 2);
-		writeSurgeData("Logger.csv", "Motor3.txt", 3);
-		writeSurgeData("Logger.csv", "Motor4.txt", 4);
-		writeSurgeData("Logger.csv", "Motor5.txt", 5);
-		writeSurgeData("Logger.csv", "Motor6.txt", 6);
-		writeSurgeData("Logger.csv", "Motor7.txt", 7);*/
+		writeSurgeData("Logger.csv", "Motor1.csv", 1);
+		writeSurgeData("Logger.csv", "Motor2.csv", 2); 
+		writeSurgeData("Logger.csv", "Motor3.csv", 3); 
+		writeSurgeData("Logger.csv", "Motor4.csv", 4);
+		writeSurgeData("Logger.csv", "Motor5.csv", 5); 
+		writeSurgeData("Logger.csv", "Motor6.csv", 6); 
+		writeSurgeData("Logger.csv", "Motor7.csv", 7);
 	}
 
 }
